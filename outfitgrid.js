8ae8024b5668090bef79a5f705663c8fd493b0ed
anaajesh <anaghaajesh2010@gmail.com>
2024-02-24 13:00:05 -0500

    random

---
+++ outfitgrid.js
@@ -0,0 +1,11 @@
+import {tempValue} from './app.js';
+
+const cellList = [];
+let rowCounter = tempValue;
+
+
+var temp = document.querySelector('.temp');
+temp.innerHTML = tempValue;
+
+var t = document.getElementById("grid");
+
