8613b2b411a989f037484231c096f5f265287ef1
hubhacker <shreyathirumala@gmail.com>
2024-02-24 13:11:04 -0500

    Attempted to implement audio

--- sounds/homeAudio.js
+++ sounds/homeAudio.js
@@ -1,9 +1,9 @@
 function playBackground() {
-    let audio = new Audio();
-          //Source goes here:
-          audio.src = "sounds/happy-monkey-circle.mp3";
-            audio.play();
-            audio.loop = true;
+  var audio = new Audio('audio_file.mp3');
+  audio.play();
+  audio.loop = true;
+
+ 
           
   }
     
